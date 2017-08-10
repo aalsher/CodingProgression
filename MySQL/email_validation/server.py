6b354e870d23ec633cdc0b69f128c7d086409e90
aalsher <amina.alsherif@gmail.com>
2017-08-10 10:42:44 -0400

    Replace email_validation server.py with edits to add email database presence validation

--- MySQL/email_validation/server.py
+++ MySQL/email_validation/server.py
@@ -25,14 +25,44 @@ def success():
 
     if EMAIL_REGEX.match(request.form['email']):
         print "Success"
-        flash(("The email address you entered {} is a VALID email address! Thank you!").format(request.form['email']))
-        query = "INSERT INTO emails (email, created_at) VALUES (:email, NOW())"
+        v_query = "SELECT * FROM emails WHERE email = :email"
         data = {'email': request.form['email'], 'created_at': now}
-        emails = mysql.query_db(query,data)
-        query = "SELECT * from emails"
-        emails = mysql.query_db(query)
+        if mysql.query_db(v_query,data):
+            print "email already exists"
+            flash("The email address you entered already exists")
+            return redirect('/')
+
+        else:
+            flash(("The email address you entered {} is a VALID email address! Thank you!").format(request.form['email']))
+            query = "INSERT INTO emails (email, created_at) VALUES (:email, NOW())"
+            data = {'email': request.form['email'], 'created_at': now}
+            emails = mysql.query_db(query,data)
+            query = "SELECT * from emails"
+            emails = mysql.query_db(query)
+            return render_template('success.html', all_emails = emails)
+
+# @app.route('/test', methods=['POST'])
+# def success():
+#     EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+#     email = request.form['email']
+#     now = str(datetime.today().strftime("%m/%d/%y %I:%M %p"))
+#     print now
+
+#     if not EMAIL_REGEX.match(request.form['email']):
+#         print "Error"
+#         flash("Email is not valid!")
+#         return redirect('/')
+
+#     if EMAIL_REGEX.match(request.form['email']):
+#         print "Success"
+#         flash(("The email address you entered {} is a VALID email address! Thank you!").format(request.form['email']))
+#         query = "INSERT INTO emails (email, created_at) VALUES (:email, NOW())"
+#         data = {'email': request.form['email'], 'created_at': now}
+#         emails = mysql.query_db(query,data)
+#         query = "SELECT * from emails"
+#         emails = mysql.query_db(query)
 
         
-        return render_template('success.html', all_emails = emails)
+#         return render_template('success.html', all_emails = emails)
     
 app.run(debug=True)
