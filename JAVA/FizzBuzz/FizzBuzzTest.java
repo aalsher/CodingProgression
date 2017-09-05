bd4d3166f9a15bb4de87795b21881b769cfbcd42
aalsher <amina.alsherif@gmail.com>
2017-09-05 13:04:34 -0400

    Add FizzBuzz

---
+++ JAVA/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+public class FizzBuzzTest {
+  public static void main(String [] args) {
+    FizzBuzz iD = new FizzBuzz();
+    String fizzbuzztest = iD.FizzBuzz(15);
+    String fizzbuzztest2 = iD.FizzBuzz(5);
+    String fizzbuzztest3 = iD.FizzBuzz(12);
+    System.out.println(fizzbuzztest);
+    System.out.println(fizzbuzztest2);
+    System.out.println(fizzbuzztest3);
+  }
+}
