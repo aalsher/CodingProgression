42e3a70fc7be2fc6b3360725b2fee8c6bac7e07d
Fifi2 <amina.alsherif@gmail.com>
2017-09-20 09:48:58 -0400

    Add daily algo

---
+++ Javascript/isLoop.js
@@ -0,0 +1,17 @@
+function isLoop(SLL){
+  if(!this.head || !this.head.next){
+    return False;
+  }
+  var slow = this.head;
+  var speedy = this.head.next;
+  while(speedy != null){
+    if(slow === speedy){
+      return true;
+    }
+    else {
+      slow = slow.next;
+      speedy = speedy.next.next;
+    }
+  }
+  return False;
+}
